91312ad0218b91686a6fc63a3abe0467f0d5f25f
Vikr-182 <dewangan.vikrant600@gmail.com>
2019-04-15 20:53:18 +0530

    made small changes

--- app.py
+++ app.py
@@ -9,27 +9,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
    
-class Quizzes(db.Model):
-  
-    __tablename__='contextfree-grammer-question'
-
-    id=db.Column(db.Integer, primary_key=True)
-    question=db.Column(db.String(200),unique=True, nullable=False)
-    answer=db.Column(db.String(100), unique=True, nullable=False)
-
-    def __init__(self,question,answer):
-        self.question = question
-        self.answer = answer
-    
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-class Quizzesmcq(db.Model):
-
-    __tablename__='contextfree-grammer-question'
-    wrong_answer1=db.Column(db.String(100),unique=True, nullable=True)
-    wrong_answer2=db.Column(db.String(100),unique=True, nullable=True)
-    computersystemsquestion_id=db.Column(db.Integer, db.ForeignKey('context-free-grammer-question.id'))
 
 
 @app.route("/")
@@ -56,9 +35,9 @@ def experiment():
 def procedure():
     return render_template("Procedure.html")
 
-@app.route("/References.html")
+@app.route("/Further Readings.html")
 def references():
-    return render_template("References.html")
+    return render_template("Further Readings.html")
 
 @app.route("/Quizzes.html")
 def quizzes():
